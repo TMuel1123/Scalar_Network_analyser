dc90760dc38f77d95d9de1967c241b64813b09fb
Thomas <TMuel1123@gmail.com>
2015-11-07 01:03:56 +0100

    Changed Serial Speed and added FastPins.h

--- Arduino_sketch/dds_powermeter/dds_powermeter.ino
+++ Arduino_sketch/dds_powermeter/dds_powermeter.ino
@@ -1,16 +1,22 @@
 //AD9850 DDS and AD8307 power meter
 // sept 2013 RH
 
+#include "FastPins.h"
+
 #define DDS_CLOCK 125000000
 
-#define  CLOCK  A2  //pin connections for DDS
-#define  LOAD A3 
-#define  DATA  A5
-#define  RESET A4
+// Pin connections for DDS
+#define  DDSPort PORTB
+#define  DDSioReg DDRB
+#define  RESET  PB0    // PB0  Pin8
+#define  LOAD   PB1    // PB1  Pin9
+#define  CLOCK  PB2    // PB2  Pin10
+#define  DATA   PB3    // PB3  Pin11
 
-int AD8307 = A7;    // select the analog input pin for AD8307
+int AD8307 = A1;    // select the analog input pin for AD8307
 
 #define arduinoLED 13   // Arduino LED on board
+#define LED PB5
 
 char inputcmd[100];  // serial data input
 int cmdindex=0;
@@ -20,13 +26,9 @@ void setup()
   pinMode(arduinoLED, OUTPUT);      // Configure the onboard LED for output
   digitalWrite(arduinoLED, LOW);    // default to LED off
   analogReference(EXTERNAL);  // 3.3v regulator output used as reference - 3.38V on this NANO board
-  Serial.begin(9600);
+  Serial.begin(57600);
   
  // init dds 
-  pinMode (DATA,  OUTPUT); 
-  pinMode (CLOCK, OUTPUT); 
-  pinMode (LOAD,  OUTPUT); 
-  pinMode (RESET, OUTPUT); 
   AD9850_init();
   AD9850_reset();
 
@@ -51,7 +53,10 @@ void loop()   // Arduino superloop - where everything gets done
       if ((temp=atol(inputcmd)) > 0) {
         SetFrequency(temp);
       }
-      else dBm_power();  
+      else
+      {
+        dBm_power();
+      }
       //else raw_power();  // python has trouble with floats
       cmdindex=0; // reset command line      
     }
@@ -65,14 +70,16 @@ void loop()   // Arduino superloop - where everything gets done
 // with the 3.38V ref the output will be 3.38V/.025V/1024 or 0.13203125db/bit
 
 void dBm_power() {
+digitalFastWrite (DDSPort, LED, HIGH);
   float dBm=0;
   int i;
   for (i=0;i<10;++i)  dBm+=(float)analogRead(AD8307); // average a few raw readings
   dBm=dBm/i;
 //  0db = 2.1v = 636 with 3.38V ref
 // -74dbm = .25v, 1dbm = .025v. if the AD8307 has an offset error we correct that here as well
-   dBm = dBm - 667;
-   dBm = dBm * 0.13203125 ;    
+   dBm = dBm - 679;
+   dBm = dBm * 0.12891 ;    
+digitalFastWrite (DDSPort, LED, LOW);
   Serial.print(" "); // may help Python parser
   Serial.print(dBm);
   Serial.println(" "); // may help Python parser
@@ -89,7 +96,7 @@ void SetFrequency(unsigned long frequency)
   unsigned long tuning_word;
   float tuneword=(frequency * pow(2, 32)) / (unsigned long) DDS_CLOCK; // hack to avoid overflow on top term
   tuning_word=(unsigned long) tuneword;
-  digitalWrite (LOAD, LOW); 
+  digitalFastWrite (DDSPort, LOAD, LOW); 
 /*  
   shiftOut(DATA, CLOCK, MSBFIRST, 0x0);
   shiftOut(DATA, CLOCK, MSBFIRST, tuning_word >> 24);
@@ -98,23 +105,28 @@ void SetFrequency(unsigned long frequency)
   shiftOut(DATA, CLOCK, MSBFIRST, tuning_word);
 */
  
-  shiftOut(DATA, CLOCK, LSBFIRST, tuning_word);
-  shiftOut(DATA, CLOCK, LSBFIRST, tuning_word >> 8);
-  shiftOut(DATA, CLOCK, LSBFIRST, tuning_word >> 16);
-  shiftOut(DATA, CLOCK, LSBFIRST, tuning_word >> 24);
-  shiftOut(DATA, CLOCK, LSBFIRST, 0x0);
+  shiftFastOut(&DDSPort, DATA, CLOCK, LSBFIRST, tuning_word);
+  shiftFastOut(&DDSPort, DATA, CLOCK, LSBFIRST, tuning_word >> 8);
+  shiftFastOut(&DDSPort, DATA, CLOCK, LSBFIRST, tuning_word >> 16);
+  shiftFastOut(&DDSPort, DATA, CLOCK, LSBFIRST, tuning_word >> 24);
+  shiftFastOut(&DDSPort, DATA, CLOCK, LSBFIRST, 0x0);
 
   
-  digitalWrite (LOAD, HIGH); 
+  digitalFastWrite (DDSPort, LOAD, HIGH); 
 }
 
 void AD9850_init()
 {
+  // Set the IO
+  digitalFastWrite(DDSioReg, RESET, OUTPUT);
+  digitalFastWrite(DDSioReg, CLOCK, OUTPUT);
+  digitalFastWrite(DDSioReg, LOAD, OUTPUT);
+  digitalFastWrite(DDSioReg, DATA, OUTPUT);
 
-  digitalWrite(RESET, LOW);
-  digitalWrite(CLOCK, LOW);
-  digitalWrite(LOAD, LOW);
-  digitalWrite(DATA, LOW);
+  digitalFastWrite(DDSPort, RESET, LOW);
+  digitalFastWrite(DDSPort, CLOCK, LOW);
+  digitalFastWrite(DDSPort, LOAD, LOW);
+  digitalFastWrite(DDSPort, DATA, LOW);
 }
 
 void AD9850_reset()
@@ -128,31 +140,28 @@ void AD9850_reset()
   // data sheet diagrams show only RESET and CLOCK being used to reset the device, but I see no output unless I also
   // toggle the LOAD line here.
 
-  digitalWrite(CLOCK, LOW);
-  digitalWrite(LOAD, LOW);
+  digitalFastWrite(DDSPort, CLOCK, LOW);
+  digitalFastWrite(DDSPort, LOAD, LOW);
 
-  digitalWrite(RESET, LOW);
-  delay(5);
-  digitalWrite(RESET, HIGH);  //pulse RESET
-  delay(5);
-  digitalWrite(RESET, LOW);
-  delay(5);
+  digitalFastWrite(DDSPort, RESET, LOW);
+  delay(1);
+  digitalFastWrite(DDSPort, RESET, HIGH);  //pulse RESET
+  delay(1);
+  digitalFastWrite(DDSPort, RESET, LOW);
+  delay(1);
 
-  digitalWrite(CLOCK, LOW);
-  delay(5);
-  digitalWrite(CLOCK, HIGH);  //pulse CLOCK
-  delay(5);
-  digitalWrite(CLOCK, LOW);
-  delay(5);
-  digitalWrite(DATA, LOW);    //make sure DATA pin is LOW
+  digitalFastWrite(DDSPort, CLOCK, LOW);
+  delay(1);
+  digitalFastWrite(DDSPort, CLOCK, HIGH);  //pulse CLOCK
+  delay(1);
+  digitalFastWrite(DDSPort, CLOCK, LOW);
+  delay(1);
+  digitalFastWrite(DDSPort, DATA, LOW);    //make sure DATA pin is LOW
 
-    digitalWrite(LOAD, LOW);
-  delay(5);
-  digitalWrite(LOAD, HIGH);  //pulse LOAD
-  delay(5);
-  digitalWrite(LOAD, LOW);
+  digitalFastWrite(DDSPort, LOAD, LOW);
+  delay(1);
+  digitalFastWrite(DDSPort, LOAD, HIGH);  //pulse LOAD
+  delay(1);
+  digitalFastWrite(DDSPort, LOAD, LOW);
   // Chip is RESET now
 }
-
-
-
